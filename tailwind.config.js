267f44d7ec439cc66253b712108a4f9cf45df158
00lar <eduardani84@gmail.com>
2023-01-24 00:44:06 -0700

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  tailwindConfig: "./tailwind.config.js",
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: '#cc2d4b',
+        secondary: '#8fa206',
+        terciary: '#61AEC9',
+      },
+      textColor: {
+        primary: '#cc2d4b',
+        secondary: '#8fa206',
+        terciary: '#61AEC9',
+      },
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
